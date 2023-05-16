5e8affabd2520563cbafecd7012eff317eea6398
Micah <ismiki977@gmail.com>
2023-05-16 17:29:08 +0300

    Loops

---
+++ Loops/DoWhileLoop.java
@@ -0,0 +1,14 @@
+
+class DoWhileLoop
+{
+    public static void main(String args[])
+    {
+
+        //do while loop will exexcute once even if the condition passed isn't true
+        int num = 3;
+        do{
+            System.out.println("Hello DO WHILE Loop" + num);
+            num++;
+        }while(num <= 5);
+    }
+}
---
+++ Loops/ForLoop.java
@@ -0,0 +1,7 @@
+ class ForLoop
+ {
+    public static void main(String args[])
+    {
+        
+    }
+ }
